5efcdf0aab5b2d64dac107c23745a842e0add283
Natnael <natnaelmehari77@gmail.com>
2021-12-02 12:56:59 +0300

    functions

---
+++ functions.c
@@ -0,0 +1,22 @@
+#include "monty.h"
+
+/**
+ * push - push element into the stack
+ * @stack
+ * @line_number
+ */
+void push(stack_t **stack, unsigned int line_number)
+{
+
+}
+
+/**
+ * pall - print elements
+ * @stack
+ * @line_number
+ */
+
+void pall(stack_t **stack, unsigned int line_number)
+{
+
+}
